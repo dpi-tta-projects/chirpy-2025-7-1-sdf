600c1a06e188d09ee9bb63d8a8b2f234b5cd323d
Ian Heraty <heratyian@gmail.com>
2025-09-25 15:37:16 -0500

    Stub out user crud controller and actions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  # TODO
+  def show; end
+
+  # TODO
+  def new; end
+  def create; end
+
+  # TODO
+  def edit; end
+  def update; end
+
+  # TODO
+  def destroy; end
+end
