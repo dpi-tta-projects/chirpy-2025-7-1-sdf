14d95ccc827cd5be48594505b42cc5a6dbd7af2a
Ian Heraty <heratyian@gmail.com>
2025-10-23 15:16:20 -0500

    Add feed preference to users

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,14 @@ Rails.application.routes.draw do
   root "pages#landing"
 
   resources :follows
-  resources :chirps do
-    collection do
-      get "following"
-    end
-  end
+  resources :chirps
   resource :session
   resources :passwords, param: :token
   resources :users do
     member do
       get "following"
       get "followers"
+      patch "feed_preference"
     end
   end
 end
