53ac24d26e8fe4e2fb0bc7b9d4b519619fa8eb75
Ian Heraty <heratyian@gmail.com>
2026-02-05 15:59:35 -0600

    Refactor favorite user query so it doesn't load users into ruby unnecessarily and does work in sql.

--- app/jobs/favorite_user_job.rb
+++ app/jobs/favorite_user_job.rb
@@ -4,10 +4,13 @@ class FavoriteUserJob < ApplicationJob
   def perform(*args)
     puts "Finding favorite user..."
 
-    # TODO: optimize
-    user_id_likes_count_hash = Like.group(:user_id).count
-    favorite_user_id = user_id_likes_count_hash.key(user_id_likes_count_hash.values.max)
-    favorite_user = User.find_by(id: favorite_user_id)
+    # find user who has liked the most chirps
+    favorite_user = User
+      .joins(:likes)
+      .group("users.id")
+      .order("COUNT(likes.id) DESC")
+      .limit(1)
+      .first
 
     puts "#{favorite_user.username} is our favorite user"
     puts "#{favorite_user.username} has liked #{favorite_user.likes.count} chirps"
